ea40464454c679019f1eecd89f15caad7939b097
Efe Genç <efegenc44@hotmail.com>
2022-12-31 13:36:39 +0300

    test rename

--- src/tests.rs
+++ src/tests.rs
@@ -2,121 +2,121 @@
 mod tests {
     #[test]
     fn factorial() {
-        let res = crate::from_file(String::from("tests/fact.dipl"));
+        let res = crate::from_file(String::from("tests/fact.belki"));
         assert!(res != None);
     }
     
     #[test]
     fn fibonacci() {
-        let res = crate::from_file(String::from("tests/fib.dipl"));
+        let res = crate::from_file(String::from("tests/fib.belki"));
         assert!(res != None);
     }
     
     #[test]
     fn math_lib() {
-        let res = crate::from_file(String::from("tests/math_lib.dipl"));
+        let res = crate::from_file(String::from("tests/math_lib.belki"));
         assert!(res != None);
     }
 
     #[test]
     fn some_test() {
-        let res = crate::from_file(String::from("tests/some_test.dipl"));
+        let res = crate::from_file(String::from("tests/some_test.belki"));
         assert!(res != None);
     }
 
     #[test]
     fn closure() {
-        let res = crate::from_file(String::from("tests/closure.dipl"));
+        let res = crate::from_file(String::from("tests/closure.belki"));
         assert!(res != None);
     }
     
     #[test]
     fn import_class_mix() {
-        let res = crate::from_file(String::from("tests/import_class_mix.dipl"));
+        let res = crate::from_file(String::from("tests/import_class_mix.belki"));
         assert!(res != None);
     }
 
     #[test]
     fn while_loop() {
-        let res = crate::from_file(String::from("tests/while_loop.dipl"));
+        let res = crate::from_file(String::from("tests/while_loop.belki"));
         assert!(res != None);
     }
 
     #[test]
     fn fizzbuzz_step() {
-        let res = crate::from_file(String::from("tests/fizzbuzz_step.dipl"));
+        let res = crate::from_file(String::from("tests/fizzbuzz_step.belki"));
         assert!(res != None);
     }
 
     #[test]
     fn return_while() {
-        let res = crate::from_file(String::from("tests/return_while.dipl"));
+        let res = crate::from_file(String::from("tests/return_while.belki"));
         assert!(res != None);
     }
 
     #[test]
     fn return_if() {
-        let res = crate::from_file(String::from("tests/return_if.dipl"));
+        let res = crate::from_file(String::from("tests/return_if.belki"));
         assert!(res != None);
     }
 
     #[test]
     fn return_block() {
-        let res = crate::from_file(String::from("tests/return_block.dipl"));
+        let res = crate::from_file(String::from("tests/return_block.belki"));
         assert!(res != None);
     }
 
     #[test]
     fn newline_awareness() {
-        let res = crate::from_file(String::from("tests/newline_awareness.dipl"));
+        let res = crate::from_file(String::from("tests/newline_awareness.belki"));
         assert!(res != None);
     }
 
     #[test]
     fn for_test() {
-        let res = crate::from_file(String::from("tests/for_test.dipl"));
+        let res = crate::from_file(String::from("tests/for_test.belki"));
         assert!(res != None);
     }
 
     #[test]
     fn while_break() {
-        let res = crate::from_file(String::from("tests/while_break.dipl"));
+        let res = crate::from_file(String::from("tests/while_break.belki"));
         assert!(res != None);
     }
 
     #[test]
     fn module_test() {
-        let res = crate::from_file(String::from("tests/module_test.dipl"));
+        let res = crate::from_file(String::from("tests/module_test.belki"));
         assert!(res != None);
     }
     
     #[test]
     fn map() {
-        let res = crate::from_file(String::from("tests/map.dipl"));
+        let res = crate::from_file(String::from("tests/map.belki"));
         assert!(res != None);
     }
 
     #[test]
     fn error() {
-        let res = crate::from_file(String::from("tests/error.dipl"));
+        let res = crate::from_file(String::from("tests/error.belki"));
         assert!(res != None);
     }
 
     #[test]
     fn ifexpr() {
-        let res = crate::from_file(String::from("tests/ifexpr.dipl"));
+        let res = crate::from_file(String::from("tests/ifexpr.belki"));
         assert!(res != None);
     }
 
     #[test]
     fn chained_relation() {
-        let res = crate::from_file(String::from("tests/chained_relation.dipl"));
+        let res = crate::from_file(String::from("tests/chained_relation.belki"));
         assert!(res != None);
     }
 
     #[test]
     fn return_for() {
-        let res = crate::from_file(String::from("tests/return_for.dipl"));
+        let res = crate::from_file(String::from("tests/return_for.belki"));
         assert!(res != None);
     }
 }
